c7af2d5c92ef5a9c3caefc9a07ebd9ee5658097b
Jansen Erdei <jansenerdei@gmail.com>
2026-06-08 20:09:47 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,40 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
 async function status(request, response) {
-    const result = await database.query("SELECT 1 + 1 as sum;")
-    console.log(result.rows)
-    response.status(200).json({chave: "valor"})
-} 
+  const updateAt = new Date().toISOString();
+  const postgresVersion = await database.getVersion();
 
-export default status
+  const dataBaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const dataBaseMaxConnections = parseInt(
+    dataBaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dataBaseConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+  console.log("dataBaseConnectionsValue:", dataBaseConnectionsValue);
+  const dataBaseOpenedConnections =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+  console.log("dataBaseOpenedConnections:", dataBaseOpenedConnections);
+  // const v = await database.query("SHOW all;");
+  // console.log(v);
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: dataBaseMaxConnections,
+        opened_connections: dataBaseOpenedConnections,
+      },
+    },
+  });
+}
+
+export default status;
