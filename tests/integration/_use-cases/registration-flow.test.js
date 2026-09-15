02844946d66427662702a63ca892b806b189f2c1
Jansen Erdei <jansenerdei@gmail.com>
2026-09-15 17:11:20 -0300

    feat: require `read:session` ro access `/user` endpoint

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -10,6 +10,8 @@ beforeAll(async () => {
   await orchestrator.deleteAllEmail();
 });
 
+let createSessionResponseBody;
+
 describe("Use case: Registration Floww (all successful)", () => {
   let createUserResponseBody;
   let activationTokenId;
@@ -99,10 +101,22 @@ describe("Use case: Registration Floww (all successful)", () => {
 
     expect(createSessionResponse.status).toBe(201);
 
-    const createSessionResponseBody = await createSessionResponse.json();
+    createSessionResponseBody = await createSessionResponse.json();
 
     expect(createSessionResponseBody.user_id).toBe(createUserResponseBody.id);
   });
 
-  test("Get user information", async () => {});
+  test("Get user information", async () => {
+      const userResponse = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          cookie: `session_id=${createSessionResponseBody.token}`,
+        },
+      });
+
+    expect(userResponse.status).toBe(200);
+
+    const userResponseBody = await userResponse.json();
+
+    expect(userResponseBody.id).toBe(createUserResponseBody.id);
+  });
 });
--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -10,6 +10,22 @@ beforeAll(async () => {
 });
 
 describe("GET /api/v1/user", () => {
+  describe("Anonymous user", () => {
+    test("Retriving endpoint", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/user");
+
+      expect(response.status).toBe(403);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "ForbiddenError",
+        message: "Você não possui permissão para executar esta ação.",
+        action: 'Verifique se o seu usuário possui a feature "read:session"',
+        status_code: 403,
+      });
+    });
+  });
   describe("Default User", () => {
     test("With valid session", async () => {
       const createdUser = await orchestrator.createUser({
